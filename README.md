e91539654678c4be50e2d5fb4f19a18f2ac62415
X1r0z <i@exp10it.cn>
2022-08-18 22:09:33 +0800

    'init'

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # 2022_Safety_Education_Auto
-南京邮电大学 2022 新生安全教育自动化脚本.
+*南京邮电大学 2022 新生安全教育自动化脚本*
+
+程序默认只完成 "2022级新生安全教育" 部分的内容, 若想完成其它课程可自行抓包更改 `userProjectId` 的值
+
+每个请求之间默认间隔 30s (小于 30s 的可行性尚未测试)
+
+## Requriements
+
+Python 3+
+
+httpx
+
+```bash
+pip3 install httpx
+```
+
+## Usage
+
+使用前请打开 auto.py, 更改以下两项的值
+
+```python
+COOKIE_SERVERID = 'YOUR_COOKIE'
+
+X_TOKEN = 'X_TOKEN'
+```
+
+获取方法见下图
+
+![pic.jpg](./pic.jpg)
+
+或者用 F12 开发者工具, 在 "网络" 一栏中查看
---
+++ auto.py
@@ -0,0 +1,98 @@
+import httpx
+import json
+import time
+
+client = httpx.Client(http2=True, verify=False)
+
+COOKIE_SERVERID = 'YOUR_COOKIE'
+
+X_TOKEN = 'X_TOKEN'
+
+cookies =  {
+    'SERVERID': COOKIE_SERVERID
+}
+
+headers = {
+    'X-Token': X_TOKEN,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54',
+    'Origin': 'https://weiban.mycourse.cn',
+    'Referer': 'https://weiban.mycourse.cn/'
+}
+
+def listCategory():
+    url = 'https://weiban.mycourse.cn/pharos/usercourse/listCategory.do'
+    data = {
+        'userProjectId': '2307c69c-76c5-404b-ad5e-624d5435c010',
+        'chooseType': '3',
+        'userId': '32462b31-fe04-49af-ad09-f50452a9994a',
+        'tenantCode': '21000008'
+    }
+    res = client.post(url, data=data, cookies=cookies,headers=headers)
+    categories = json.loads(res.text)
+    for i in categories['data']:
+        print(i['categoryName'],i['categoryCode'])
+        yield i['categoryName'],i['categoryCode']
+
+def listCourse(categoryCode):
+    url = 'https://weiban.mycourse.cn/pharos/usercourse/listCourse.do'
+    data = {
+        'userProjectId': '2307c69c-76c5-404b-ad5e-624d5435c010',
+        'chooseType': '3',
+        'categoryCode': categoryCode,
+        'name': '',
+        'userId': '32462b31-fe04-49af-ad09-f50452a9994a',
+        'tenantCode': '21000008'
+    }
+    res = client.post(url, data=data, cookies=cookies,headers=headers)
+    courses = json.loads(res.text)
+    for i in courses['data']:
+        print('\t',i['resourceName'], isFinished(i['finished']))
+        yield i['resourceName'], i['resourceId'], i['userCourseId'], i['finished']
+
+def isFinished(finished):
+    if finished == 1:
+        return '已完成'
+    else:
+        return '未完成'
+
+def studyCourse(courseId):
+    url = 'https://weiban.mycourse.cn/pharos/usercourse/study.do'
+    data = {
+        'courseId': courseId,
+        'userProjectId': '2307c69c-76c5-404b-ad5e-624d5435c010',
+        'tenantCode': '21000008',
+        'userId': '32462b31-fe04-49af-ad09-f50452a9994a'
+    }
+    res = client.post(url, data=data, cookies=cookies,headers=headers)
+
+def getCourseUrl(courseId):
+    url = 'https://weiban.mycourse.cn/pharos/usercourse/getCourseUrl.do'
+    data = {
+        'courseId': courseId,
+        'userProjectId': '2307c69c-76c5-404b-ad5e-624d5435c010',
+        'tenantCode': '21000008',
+        'userId': '32462b31-fe04-49af-ad09-f50452a9994a',
+    }
+    res = client.post(url, data=data, cookies=cookies,headers=headers)
+
+def finishCourse(userCourseId):
+    url = 'https://weiban.mycourse.cn/pharos/usercourse/finish.do'
+    params = {
+        'callback': '',
+        'userCourseId': userCourseId,
+        'tenantCode': '21000008'
+    }
+    res = client.get(url, params=params, cookies=cookies,headers=headers)
+    #print(res.text)
+
+
+while True:
+    for _, categoryCode in listCategory():
+        for _, courseId, userCourseId, finished in listCourse(categoryCode):
+            if finished == 1:
+                continue
+            else:
+                #studyCourse(courseId)
+                #getCourseUrl(courseId)
+                time.sleep(30)
+                finishCourse(userCourseId)
